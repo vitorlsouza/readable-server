b7af75e10e9b2e3ea43c4a1d948286c6a3a690e6
Vitor <vitorlopesdesouza@gmail.com>
2018-08-13 13:31:48 -0300

    first commit

---
+++ posts.js
@@ -0,0 +1,162 @@
+const clone = require('clone')
+
+let db = {}
+
+const defaultData = {
+  "8xf0y6ziyjabvozdd253nd": {
+    id: '8xf0y6ziyjabvozdd253nd',
+    timestamp: 1467166872634,
+    title: 'Udacity is the best place to learn React',
+    body: 'Everyone says so after all.Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.',
+    author: 'thingtwo',
+    category: 'react',
+    voteScore: 6,
+    deleted: false,
+    commentCount: 2
+  },
+  "8xf0y6ziyjabvozdd253ne": {
+    id: '8xf0y6ziyjabvozdd253ne',
+    timestamp: 1467166872634,
+    title: 'Udacity is the best place to learn React 2',
+    body: 'Everyone says so after all.',
+    author: 'thingtwo',
+    category: 'react',
+    voteScore: 7,
+    deleted: false,
+    commentCount: 0
+  },
+  "8xf0y6ziyjabvozdd253nf": {
+    id: '8xf0y6ziyjabvozdd253nf',
+    timestamp: 1467166872634,
+    title: 'Udacity is the best place to learn React 3',
+    body: 'Everyone says so after all.',
+    author: 'thingtwo',
+    category: 'react',
+    voteScore: 8,
+    deleted: false,
+    commentCount: 0
+  },
+  "6ni6ok3ym7mf1p33lnez": {
+    id: '6ni6ok3ym7mf1p33lnez',
+    timestamp: 1468479767190,
+    title: 'Learn Redux in 10 minutes!',
+    body: 'Just kidding. It takes more than 10 minutes to learn technology.',
+    author: 'thingone',
+    category: 'redux',
+    voteScore: -5,
+    deleted: false,
+    commentCount: 0
+  }
+}
+
+function getData (token) {
+  let data = db[token]
+  if (data == null) {
+    data = db[token] = clone(defaultData)
+  }
+  return data
+}
+
+function getByCategory (token, category) {
+  return new Promise((res) => {
+    let posts = getData(token)
+    let keys = Object.keys(posts)
+    let filtered_keys = keys.filter(key => posts[key].category === category && !posts[key].deleted)
+    res(filtered_keys.map(key => posts[key]))
+  })
+}
+
+function get (token, id) {
+  return new Promise((res) => {
+    const posts = getData(token)
+    res(
+      posts[id].deleted
+        ? {}
+        : posts[id]
+    )
+  })
+}
+
+function getAll (token) {
+  return new Promise((res) => {
+    const posts = getData(token)
+    let keys = Object.keys(posts)
+    let filtered_keys = keys.filter(key => !posts[key].deleted)
+    res(filtered_keys.map(key => posts[key]))
+  })
+}
+
+function add (token, post) {
+  return new Promise((res) => {
+    let posts = getData(token)
+
+    posts[post.id] = {
+      id: post.id,
+      timestamp: post.timestamp,
+      title: post.title,
+      body: post.body,
+      author: post.author,
+      category: post.category,
+      voteScore: 1,
+      deleted: false,
+      commentCount: 0
+    }
+
+    res(posts[post.id])
+  })
+}
+
+function vote (token, id, option) {
+  return new Promise((res) => {
+    let posts = getData(token)
+    post = posts[id]
+    switch(option) {
+        case "upVote":
+            post.voteScore = post.voteScore + 1
+            break
+        case "downVote":
+            post.voteScore = post.voteScore - 1
+            break
+        default:
+            console.log(`posts.vote received incorrect parameter: ${option}`)
+    }
+    res(post)
+  })
+}
+
+function disable (token, id) {
+    return new Promise((res) => {
+      let posts = getData(token)
+      posts[id].deleted = true
+      res(posts[id])
+    })
+}
+
+function edit (token, id, post) {
+    return new Promise((res) => {
+        let posts = getData(token)
+        for (prop in post) {
+            posts[id][prop] = post[prop]
+        }
+        res(posts[id])
+    })
+}
+
+function incrementCommentCounter(token, id, count) {
+  const data = getData(token)
+  if (data[id]) {
+    data[id].commentCount += count
+  }
+}
+
+module.exports = {
+  get,
+  getAll,
+  getByCategory,
+  add,
+  vote,
+  disable,
+  edit,
+  getAll,
+  incrementCommentCounter
+}
